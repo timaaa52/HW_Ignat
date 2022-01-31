6f1b3887b9f4b9ae5d2470270b49bac31a661ce1
Tima <tima52rus@gmail.com>
2022-01-31 04:35:17 +0200

    complete HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './pages/header.module.css'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <input type="checkbox" id="hmt" className={s.hiddenMenuTicker}/>
+                <label className={s.btnMenu} htmlFor="hmt">
+                    <span className={s.first}></span>
+                    <span className={s.second}></span>
+                    <span className={s.third}></span>
+                </label>
+                <ul className={s.hiddenMenu}>
+                    <li><NavLink to={'/pre-junior'}> Pre Junior </NavLink> </li>
+                    <li><NavLink to={'/junior'}>Junior </NavLink></li>
+                    <li><NavLink to={'/junior-plus'}>Junior-Plus</NavLink></li>
+                </ul>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HW12 from "../../h12/HW12";
+
+const JuniorPlus = () => {
+    return (
+        <div>
+            <HW12/>
+        </div>
+    );
+};
+
+export default JuniorPlus;
